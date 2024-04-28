ce1e6dd53eec4bdcd270439130fd89608bd6e38e
Serhii Stanislav <serhiistanislav@gmail.com>
2024-04-28 15:22:24 +0300

    ADD | env delay

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { useEffect } from 'react';
 import * as gu from './utils/global';
 import Dashboard from './components/Dashboard';
 
-const delay = 5000; // 5 sec
-// const delay = 60000 // 1 min
-// const delay = 600000; // 10 min
+const delay = process.env.REACT_APP_UPDATE_DELAY;
 
 const App = () => {
   useEffect(() => {
     gu.updatePrices();
-    const int = setInterval(() => gu.updatePrices(), delay);
+    const int = setInterval(() => gu.updatePrices(), Number(delay) || 600000);
     return () => {
       clearInterval(int);
     };
